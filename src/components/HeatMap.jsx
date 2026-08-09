b74d12d3a9ca5167d14e759fc2f743e078a7fe91
kalkiyama <297750273+kalkiyama@users.noreply.github.com>
2026-08-09 16:33:25 -0400

    restore the useRef import the revert reintroduced a need for

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Maximize2, X } from "lucide-react";
 import { C, HEAT_RAMP } from "../theme.js";
 import WorldMap from "./WorldMap.jsx";
